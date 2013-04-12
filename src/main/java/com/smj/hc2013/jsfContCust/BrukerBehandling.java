881d001a25d0652911436497e1e94c475663e666
deb <deb@deb-BN.ka.hist.no>
2013-04-12 13:29:31 +0200

    Han helvett

--- src/main/java/com/smj/hc2013/jsfContCust/BrukerBehandling.java
+++ src/main/java/com/smj/hc2013/jsfContCust/BrukerBehandling.java
@@ -5,23 +5,29 @@ package com.smj.hc2013.jsfContCust;
  * and open the template in the editor.
  */
 import com.smj.hc2013.jsfContl.util.JsfUtil;
+import com.smj.hc2013.model.Bruker;
+import com.smj.hc2013.session.BrukerFacade;
+import java.io.IOException;
 import java.io.Serializable;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.annotation.PostConstruct;
+import javax.ejb.EJB;
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 import javax.faces.context.ExternalContext;
 import javax.faces.context.FacesContext;
 import javax.persistence.Cacheable;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
 
 /**
  *
  * @author deb
  */
-@ManagedBean(name ="bruker")
+@ManagedBean(name = "bruker")
 @SessionScoped
 @Cacheable(false)
 public class BrukerBehandling implements Serializable {
@@ -31,6 +37,48 @@ public class BrukerBehandling implements Serializable {
     private boolean adminOK;
     private String rolle = "";
     private String userData = "";
+    private String username;
+    private String password;
+    private String originalURL;
+
+    @PostConstruct
+    public void init() {
+        ExternalContext externalContext = FacesContext.getCurrentInstance().getExternalContext();
+        originalURL = (String) externalContext.getRequestMap().get(RequestDispatcher.FORWARD_REQUEST_URI);
+
+        if (originalURL == null) {
+            originalURL = externalContext.getRequestContextPath() + "/index.xhtml";
+        } else {
+            String originalQuery = (String) externalContext.getRequestMap().get(RequestDispatcher.FORWARD_QUERY_STRING);
+
+            if (originalQuery != null) {
+                originalURL += "?" + originalQuery;
+            }
+        }
+    }
+    @EJB
+    private BrukerFacade userService;
+
+    public void login() throws IOException {
+        FacesContext context = FacesContext.getCurrentInstance();
+        ExternalContext externalContext = context.getExternalContext();
+        HttpServletRequest request = (HttpServletRequest) externalContext.getRequest();
+
+        try {
+            request.login(username, password);
+            Bruker user = userService.find(username);
+            externalContext.getSessionMap().put("user", user);
+            externalContext.redirect(originalURL);
+        } catch (ServletException e) {
+            context.addMessage(null, new FacesMessage("Unknown login"));
+        }
+    }
+
+    public void logout() throws IOException {
+        ExternalContext externalContext = FacesContext.getCurrentInstance().getExternalContext();
+        externalContext.invalidateSession();
+        externalContext.redirect(externalContext.getRequestContextPath() + "/index.xhtml");
+    }
 
     public void setRolle(String rolle) {
         this.rolle = rolle;
@@ -39,7 +87,6 @@ public class BrukerBehandling implements Serializable {
     public void setUserData(String userData) {
         this.userData = userData;
     }
-    
 
     public boolean isAdminOK() {
         this.adminOK = (getRolle().equals("admin")) ? true : false;
@@ -49,7 +96,6 @@ public class BrukerBehandling implements Serializable {
     public void setAdminOK(boolean adminOK) {
         this.adminOK = adminOK;
     }
-    
 
     public String getRolle() {
         for (String r : roller) {
@@ -57,55 +103,44 @@ public class BrukerBehandling implements Serializable {
                 return r;
             }
         }
-        logout();
+        try {
+            logout();
+        } catch (Exception e) {
+        }
         return "NO ROLE, logging you out!";
     }
-    
+
     public String getUserData() {
         ExternalContext context = FacesContext.getCurrentInstance().getExternalContext();
         Object forsporrselobject = context.getRequest();
         if (!(forsporrselobject instanceof HttpServletRequest)) {
             logger.log(Level.SEVERE, "Det forespurte objektet er av type {0}", forsporrselobject.getClass());
-           
+
         }
         HttpServletRequest foresporrsel = (HttpServletRequest) forsporrselobject;
         return (foresporrsel.getRemoteUser());
     }
 
-    public String logout() {
+    public String reg() {
+        return "/faces/login/registrering?faces-redirect=true";
+    }
+
+    public String logout2() {
+        String result = "/faces/login/index?faces-redirect=true";
+
         FacesContext context = FacesContext.getCurrentInstance();
         HttpServletRequest request = (HttpServletRequest) context.getExternalContext().getRequest();
-        HttpSession session = request.getSession(true);
-        session.invalidate();
 
         try {
             request.logout();
             request.logout();
         } catch (ServletException e) {
+            JsfUtil.addErrorMessage("Failes to log you our!");
+            result = "/faces/login/ikkelogin?faces-redirect=true";
         }
-        return "ok";
+
+        return result;
     }
-    
-    public String reg(){
-        return "/faces/login/registrering?faces-redirect=true";
-    }
-    
-     public String logout2() {
-    String result="/faces/login/index?faces-redirect=true";
-     
-    FacesContext context = FacesContext.getCurrentInstance();
-    HttpServletRequest request = (HttpServletRequest)context.getExternalContext().getRequest();
-     
-    try {
-      request.logout();
-      request.logout();
-    } catch (ServletException e) {
-      JsfUtil.addErrorMessage("Failes to log you our!");       
-      result = "/faces/login/ikkelogin?faces-redirect=true";
-    }
-     
-    return result;
-  }
 
     public boolean isInRole(String k) {
         ExternalContext context = FacesContext.getCurrentInstance().getExternalContext();
@@ -116,5 +151,21 @@ public class BrukerBehandling implements Serializable {
         }
         HttpServletRequest foresporrsel = (HttpServletRequest) forsporrselobject;
         return foresporrsel.isUserInRole(k);
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
     }
 }
