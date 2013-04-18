740cd69fa35d1b73326171885eb983306fad02c3
deb <deb@deb-BN.ka.hist.no>
2013-04-18 12:49:58 +0200

    Han helvett

---
+++ src/main/java/com/smj/hc2013/jsfContCust/Dishes.java
@@ -0,0 +1,48 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.smj.hc2013.jsfContCust;
+
+import com.smj.hc2013.model.Retter;
+import com.smj.hc2013.session.RetterFacade;
+import java.io.Serializable;
+import java.util.List;
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+/**
+ *
+ * @author Martin
+ */
+
+@ManagedBean
+@RequestScoped
+public class Dishes implements Serializable {
+
+    private Retter selected = new Retter();
+    private RetterFacade rettF;
+    private List<Retter> rettL;
+
+    public Retter getSelected() {
+        return selected;
+    }
+
+    public void setSelected(Retter selected) {
+        this.selected = selected;
+    }
+
+    public List<Retter> getRettL() {
+        return rettL;
+    }
+
+    public void setRettL(List<Retter> rettL) {
+        this.rettL = rettL;
+    }
+      
+    @PostConstruct
+    public void init() {
+       rettL = rettF.findAll();
+    }
+}
