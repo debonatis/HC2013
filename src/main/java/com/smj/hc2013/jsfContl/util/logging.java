3aa94560afcc64f328edde72c2d33c638cc2f8f2
deb <deb@deb-BN.ka.hist.no>
2013-04-27 14:52:09 +0200

    Han helvett

--- src/main/java/com/smj/hc2013/jsfContl/util/logging.java
+++ src/main/java/com/smj/hc2013/jsfContl/util/logging.java
@@ -4,10 +4,14 @@
  */
 package com.smj.hc2013.jsfContl.util;
 
+import com.smj.hc2013.jsfContCust.BrukerBehandling;
 import com.smj.hc2013.model.Bruker;
+import com.smj.hc2013.model.Timer;
 import com.smj.hc2013.session.BrukerFacade;
+import com.smj.hc2013.session.TimerFacade;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Date;
 import java.util.List;
 import javax.annotation.security.DeclareRoles;
 import javax.ejb.EJB;
@@ -21,27 +25,51 @@ import javax.faces.bean.ManagedBean;
 @ManagedBean
 @ApplicationScoped
 @DeclareRoles({"Admin"})
-public class logging extends BrukerTellerHelp{
+public class logging extends BrukerBehandling{
     
     @EJB
     private BrukerFacade brukerF;
-    private static List<Bruker> liste = Collections.synchronizedList(new ArrayList<Bruker>());
+    @EJB
+    private TimerFacade timerF;
+    private  List<Timer> liste = Collections.synchronizedList(new ArrayList<Timer>());
     private Bruker bruker;
+    private Timer timeO;
+    private Date start;
+    private Date stop;
 
-    public Bruker getBruker() {
+    public Date getStart() {
+        return start;
+    }
+
+    public void setStart(Date start) {
+        this.start = start;
+    }
+
+    public Date getStop() {
+        return stop;
+    }
+
+    public void setStop(Date stop) {
+        this.stop = stop;
+    }
+
+    public Bruker getBruker() {        
+      bruker =  brukerF.find(getUserData());
         return bruker;
-    }
-
-    public void setBruker(Bruker bruker) {
-        this.bruker = bruker;
+        
+    }  
+    public void regWorkHours(){
+        Date time = new Date(stop.getTime()- start.getTime());
+        timeO = new Timer();
+        timeO.setTimeId(new Date(System.currentTimeMillis()));
+        timeO.setBrukernavn(null);
+        timeO.setArbeidsTimer(time);
+        timerF.create(timeO);
     }
     
     
-    public synchronized List<Bruker> getLoggedInUsers(){
-        for(Bruker b:getBrukere()){
-          Bruker f = brukerF.find(b);
-            liste.add(f);
-        }
+    public synchronized List<Timer> getLoggedInUsers(){ 
+        liste = timerF.findAll();
         return liste;
     }
     
