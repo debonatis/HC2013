473498217ba1788718533b55e1c4dfa5897260e5
deb <deb@deb-BN.ka.hist.no>
2013-04-28 23:33:07 +0200

    Han helvett

---
+++ src/test/java/com/smj/hc2013/jsfContCust/BestillingTest.java
@@ -0,0 +1,370 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.smj.hc2013.jsfContCust;
+
+import com.smj.hc2013.model.Ordre;
+import com.smj.hc2013.model.OrdreBestilling;
+import com.smj.hc2013.model.Retter;
+import java.util.List;
+import java.util.UUID;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.primefaces.event.FlowEvent;
+import org.primefaces.event.TransferEvent;
+import org.primefaces.model.DualListModel;
+import org.primefaces.model.StreamedContent;
+
+/**
+ *
+ * @author deb
+ */
+public class BestillingTest {
+    
+    public BestillingTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of prepareCreate method, of class Bestilling.
+     */
+    @Test
+    public void testPrepareCreate() {
+        System.out.println("prepareCreate");
+        Bestilling instance = new Bestilling();
+        instance.prepareCreate();
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getOrdre method, of class Bestilling.
+     */
+    @Test
+    public void testGetOrdre() {
+        System.out.println("getOrdre");
+        Bestilling instance = new Bestilling();
+        Ordre expResult = null;
+        Ordre result = instance.getOrdre();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of setOrdre method, of class Bestilling.
+     */
+    @Test
+    public void testSetOrdre() {
+        System.out.println("setOrdre");
+        Ordre ordre = null;
+        Bestilling instance = new Bestilling();
+        instance.setOrdre(ordre);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getSettAntallList method, of class Bestilling.
+     */
+    @Test
+    public void testGetSettAntallList() {
+        System.out.println("getSettAntallList");
+        Bestilling instance = new Bestilling();
+        List expResult = null;
+        List result = instance.getSettAntallList();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getBridList method, of class Bestilling.
+     */
+    @Test
+    public void testGetBridList() {
+        System.out.println("getBridList");
+        Bestilling instance = new Bestilling();
+        String[] expResult = null;
+        String[] result = instance.getBridList();
+        assertArrayEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of setSettAntallList method, of class Bestilling.
+     */
+    @Test
+    public void testSetSettAntallList() {
+        System.out.println("setSettAntallList");
+        List<OrdreBestilling> settAntallList = null;
+        Bestilling instance = new Bestilling();
+        instance.setSettAntallList(settAntallList);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getRetter method, of class Bestilling.
+     */
+    @Test
+    public void testGetRetter() {
+        System.out.println("getRetter");
+        Bestilling instance = new Bestilling();
+        List expResult = null;
+        List result = instance.getRetter();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of setRetter method, of class Bestilling.
+     */
+    @Test
+    public void testSetRetter() {
+        System.out.println("setRetter");
+        List<Retter> retter = null;
+        Bestilling instance = new Bestilling();
+        instance.setRetter(retter);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getMaal method, of class Bestilling.
+     */
+    @Test
+    public void testGetMaal() {
+        System.out.println("getMaal");
+        Bestilling instance = new Bestilling();
+        List expResult = null;
+        List result = instance.getMaal();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of setMaal method, of class Bestilling.
+     */
+    @Test
+    public void testSetMaal() {
+        System.out.println("setMaal");
+        List<Retter> maal = null;
+        Bestilling instance = new Bestilling();
+        instance.setMaal(maal);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of init method, of class Bestilling.
+     */
+    @Test
+    public void testInit() {
+        System.out.println("init");
+        Bestilling instance = new Bestilling();
+        instance.init();
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getRetterPick method, of class Bestilling.
+     */
+    @Test
+    public void testGetRetterPick() {
+        System.out.println("getRetterPick");
+        Bestilling instance = new Bestilling();
+        DualListModel expResult = null;
+        DualListModel result = instance.getRetterPick();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of setRetterPick method, of class Bestilling.
+     */
+    @Test
+    public void testSetRetterPick() {
+        System.out.println("setRetterPick");
+        DualListModel<Retter> retterPick = null;
+        Bestilling instance = new Bestilling();
+        instance.setRetterPick(retterPick);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of byttHvisNull method, of class Bestilling.
+     */
+    @Test
+    public void testByttHvisNull() {
+        System.out.println("byttHvisNull");
+        Object objectToCheck = null;
+        Object defaultValue = null;
+        Object expResult = null;
+        Object result = Bestilling.byttHvisNull(objectToCheck, defaultValue);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of savePick method, of class Bestilling.
+     */
+    @Test
+    public void testSavePick() throws Exception {
+        System.out.println("savePick");
+        Bestilling instance = new Bestilling();
+        instance.savePick();
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of isSkip method, of class Bestilling.
+     */
+    @Test
+    public void testIsSkip() {
+        System.out.println("isSkip");
+        Bestilling instance = new Bestilling();
+        boolean expResult = false;
+        boolean result = instance.isSkip();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of setSkip method, of class Bestilling.
+     */
+    @Test
+    public void testSetSkip() {
+        System.out.println("setSkip");
+        boolean skip = false;
+        Bestilling instance = new Bestilling();
+        instance.setSkip(skip);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of isMailVe method, of class Bestilling.
+     */
+    @Test
+    public void testIsMailVe() {
+        System.out.println("isMailVe");
+        Bestilling instance = new Bestilling();
+        boolean expResult = false;
+        boolean result = instance.isMailVe();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of setMailVe method, of class Bestilling.
+     */
+    @Test
+    public void testSetMailVe() {
+        System.out.println("setMailVe");
+        boolean MailVe = false;
+        Bestilling instance = new Bestilling();
+        instance.setMailVe(MailVe);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of removeFromsetAntalle method, of class Bestilling.
+     */
+    @Test
+    public void testRemoveFromsetAntalle() {
+        System.out.println("removeFromsetAntalle");
+        OrdreBestilling item = null;
+        Bestilling instance = new Bestilling();
+        instance.removeFromsetAntalle(item);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of onFlowProcess method, of class Bestilling.
+     */
+    @Test
+    public void testOnFlowProcess() {
+        System.out.println("onFlowProcess");
+        FlowEvent event = null;
+        Bestilling instance = new Bestilling();
+        String expResult = "";
+        String result = instance.onFlowProcess(event);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of onTransfer method, of class Bestilling.
+     */
+    @Test
+    public void testOnTransfer() {
+        System.out.println("onTransfer");
+        TransferEvent event = null;
+        Bestilling instance = new Bestilling();
+        instance.onTransfer(event);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getUUID method, of class Bestilling.
+     */
+    @Test
+    public void testGetUUID() {
+        System.out.println("getUUID");
+        Bestilling instance = new Bestilling();
+        UUID expResult = null;
+        UUID result = instance.getUUID();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getFile method, of class Bestilling.
+     */
+    @Test
+    public void testGetFile() {
+        System.out.println("getFile");
+        Bestilling instance = new Bestilling();
+        StreamedContent expResult = null;
+        StreamedContent result = instance.getFile();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+}
---
+++ src/test/java/sample/Tester.java
@@ -0,0 +1,41 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sample;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+/**
+ *
+ * @author deb
+ */
+@RunWith(Suite.class)
+@Suite.SuiteClasses({})
+public class Tester {
+
+// Skønner ikke helt hvordan jeg skal bruke dette sammen JSF og Glassfish servern.
+// Tenker da på lage tester som gir mening.
+    
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+    }
+
+    @AfterClass
+    public static void tearDownClass() throws Exception {
+    }
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+    
+}
